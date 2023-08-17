10f8c1d469740bddb0d8efeda9dd2ac1368db760
Kingjiyun <etock19@naver.com>
2023-08-17 20:06:43 +0900

    탭포스트예제

---
+++ app/src/main/java/com/example/tabhosttest/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.tabhosttest
+
+import android.app.TabActivity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@Suppress("deprecation")
+class MainActivity : TabActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var tabHost = this.tabHost
+
+        var tabSpec1 = tabHost.newTabSpec("spec1").setIndicator("아이브")
+        tabSpec1.setContent(R.id.linear1)
+        tabHost.addTab(tabSpec1)
+
+        var tabSpec2 = tabHost.newTabSpec("spec2").setIndicator("누진스")
+        tabSpec2.setContent(R.id.linear2)
+        tabHost.addTab(tabSpec2)
+
+        var tabSpec3 = tabHost.newTabSpec("spec3").setIndicator("비투비")
+        tabSpec3.setContent(R.id.linear3)
+        tabHost.addTab(tabSpec3)
+
+        tabHost.currentTab = 1
+
+    }
+}
